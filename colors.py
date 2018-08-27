f68fb2ed407cf8b49235d5622503aa2982aad7b9
Sidolux <midas78@gmail.com>
2018-08-27 18:51:29 +0200

    Initial

---
+++ colors.py
@@ -0,0 +1,8 @@
+colors = ["czerwony", "zółty", "zielony", "niebieski", "czarny"]
+i = 0
+while i < len(colors):
+    print("Kiedy miałem %d lat, moim ulubionym kolorem był %s." % (i, colors[i]))
+    i = i + 1
+
+for x in colors[:]:
+    print(x)
